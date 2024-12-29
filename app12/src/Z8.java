a6276547bd4dd870e2caaa70bb2f767cce5cd40e
Ashutosh Kushwaha <92290543+Ashutosh2505@users.noreply.github.com>
2024-12-30 00:08:06 +0530

    Add files via upload

---
+++ app12/src/Z8.java
@@ -0,0 +1,12 @@
+class Z8 {
+	public static void main(String[] args) {
+		System.out.println(1);
+		if (false)
+			System.out.println(2);
+		if (true)
+			System.out.println(3);
+		if (true)
+			System.out.println(4);
+		System.out.println(5);
+	}
+}
