df22f62e9230fd5ef8bbfdfc20b8d7c2c85ed904
Ashutosh Kushwaha <92290543+Ashutosh2505@users.noreply.github.com>
2024-12-30 15:06:38 +0530

    Add files via upload

---
+++ app20/src/R1.java
@@ -0,0 +1,15 @@
+class R1 {
+	public static void main(String[] args) {
+		System.out.println("hello  " + test1(test2(40 + 30)));
+	}
+
+	public static int test1(int i) {
+		System.out.println("test1");
+		return i + 10;
+	}
+
+	public static int test2(int i) {
+		System.out.println("test2");
+		return i + 30;
+	}
+}
