5cc5309834ad5f82a236714686c7b3159c1652f5
Ashutosh Kushwaha <92290543+Ashutosh2505@users.noreply.github.com>
2025-01-01 14:43:24 +0530

    Add files via upload

---
+++ app31/src/G.java
@@ -0,0 +1,11 @@
+class G {
+	public static void main(String[] args) {
+		String[] array = new String[2]; // declaration and definition
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		array[0] = "abc";
+		array[1] = "xyz";
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+	}
+}
